cb85372c01f3319da99bc6b7977412bcd15d7467
Efe Dilmaç <48404344+SuluKarpuz@users.noreply.github.com>
2023-02-24 20:03:14 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -5,4 +5,4 @@ def telegram_bot_sendtext(bot_mesaj,id):
     bot_chatID = id
     url= "https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+str(bot_chatID)+"&parse_mode=Markdown&text="+bot_mesaj
 
-    response=requests.get(url,1033220757)
+    response=requests.get(url,)
